ad63a903f3fd666715ee25c71e82e47ac3d7d419
FeynmanFan <cbehrens@gmail.com>
2024-02-08 15:47:11 -0600

    Initial add

---
+++ CollisionDetector/Detector.cs
@@ -0,0 +1,23 @@
+﻿namespace CollisionDetector
+{
+    /// <summary>
+    /// Static class wrapper for collision logic
+    /// </summary>
+    public static class Detector
+    {
+        /// <summary>
+        /// Returns a true if the hexes overlap, false if not
+        /// </summary>
+        /// <param name="hex1">The first hex to compare</param>
+        /// <param name="hex2">The second hex to compare</param>
+        /// <returns>True if colliding, false if not</returns>
+        public static bool DetectionCollision(Hex hex1, Hex hex2)
+        {
+            var distance = Math.Sqrt(Math.Pow(hex2.Center.X - hex1.Center.X, 2) + Math.Pow(hex2.Center.Y - hex1.Center.Y, 2));
+
+            var sumOfRadii = hex1.Radius + hex2.Radius;
+
+            return distance <= sumOfRadii;
+        }
+    }
+}
---
+++ CollisionDetector/Hex.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+
+namespace CollisionDetector
+{
+    /// <summary>
+    /// The structure we use for representing objects in game
+    /// </summary>
+    public class Hex
+    {
+        /// <summary>
+        /// X and Y coordinates in the game system
+        /// </summary>
+        public PointF Center { get; set; }
+
+        /// <summary>
+        /// The size of the object represented in dimension-free coordinate units
+        /// </summary>
+        public float Radius { get; set; }
+
+        /// <summary>
+        /// Constructor for Hex object
+        /// </summary>
+        /// <param name="center">The x and y coords of the object</param>
+        /// <param name="radius">The size of the object</param>
+        private Hex(PointF center, float radius)
+        {
+            this.Center = center;
+            this.Radius = radius;
+        }
+
+        /// <summary>
+        /// Static factory method to create hex
+        /// </summary>
+        /// <param name="center">The x and y coords of the object</param>
+        /// <param name="radius">The size of the object</param>
+        /// <returns></returns>
+        public static Hex CreateHex(PointF center, float radius)
+        {
+            return new Hex(center, radius);
+        }
+    }
+}
